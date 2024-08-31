c90319adb4d23777c0c86a360bb26873f08dae04
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-31 23:41:04 +0600

    fixed the snake movement animation

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from turtle import Turtle, Screen
-
-initialPosition = [(-20, 0), (0, 0), (20, 0)]
-
-for i in initialPosition:
-    snakeSegment = Turtle()
-    snakeSegment.shape ("square")
-    snakeSegment.color("white")
-    snakeSegment.goto(i)
+import time
 
 screen = Screen ()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title ("Snake Rax")
+screen.tracer(0)
+
+initialPosition = [(-20, 0), (0, 0), (20, 0)]
+snakeSegments = []
+
+for i in initialPosition:
+    snakeSegment = Turtle()
+    snakeSegment.shape ("square")
+    snakeSegment.color("white")
+    snakeSegment.penup()
+    snakeSegment.goto(i)
+    snakeSegments.append(snakeSegment)
+
+gameOn = True
+while gameOn:
+    time.sleep(1)
+    screen.update()
+    for segment in snakeSegments:
+        segment.forward(20)
+        
+
+
+
+
 screen.exitonclick()
-
-
-
 
 
 
