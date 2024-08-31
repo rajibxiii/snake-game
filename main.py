d1d146f3dd2c09d91f4a995bf1e7f47258bf6904
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-09-01 02:35:32 +0600

    segments are moving properly

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ screen.bgcolor("black")
 screen.title ("Snake Rax")
 screen.tracer(0) # this method accelearates the complex graphics
 
-initialPosition = [(-20, 0), (0, 0), (20, 0)]
+initialPosition = [(20, 0), (0, 0), (-20, 0)] # seg1 seg2 seg3 (from right of the snake)
 snakeSegments = []
 
 for i in initialPosition: # creating three segments of a snake
@@ -22,12 +22,14 @@ gameOn = True
 while gameOn:
     time.sleep(1) # waits a second before the next update
     screen.update() # updates screen every time after the below loop is executed
-    for segment in snakeSegments:
-        segment.forward(20)
-        
 
-
-
+    for segment_number in range (len(snakeSegments)-1, 0, -1): # just a reverse loop
+        new_x = snakeSegments [segment_number-1].xcor() # getting the x coordinate of second last segment
+        new_y = snakeSegments [segment_number-1].ycor() # getting the y coordinate of second last segment
+        snakeSegments[segment_number].goto(new_x, new_y) # going to the poisition of the second last segment from the last segment
+        # all the segments are moved
+    snakeSegments[0].forward(20) # now forwarding the first segment
+    snakeSegments[0].left(90)
 
 screen.exitonclick()
 
