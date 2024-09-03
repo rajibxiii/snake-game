21aff0d89cac6a22b41bba7e28aebea44712fc27
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-09-04 02:27:37 +0600

    perfection of code ensured. project completed.

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ screen.bgcolor("black")
 screen.title ("Snake Rax")
 screen.tracer(0) # this method accelearates the complex graphics
 
+# asking for speed setting
+snakeSpeed = 0
+while snakeSpeed <= 0 or snakeSpeed >10:
+    snakeSpeed = int(screen.textinput(title="Set Snake Speed", prompt="What speed do you want for your snake? (from 1 to 10)"))
+
 snake = Snake()
 snake.makeSnake()
 food = Food()
-scores = Scoreboard(2) #TODO: prompt for speed
+scores = Scoreboard(snakeSpeed)
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,9 +27,10 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 gameOn = True
+snakeSleep = 1.1-(snakeSpeed/10) # if speed is 10, sleep is 0.1. if 6, then 0.5
 while gameOn:
     screen.update() # updates screen every time after the below loop is executed
-    time.sleep(0.3) # waits a second before the next update
+    time.sleep(snakeSleep) # waits a second before the next update
     snake.move()
 
     if snake.head.distance(food) < 15: # when snake hits the food
@@ -38,22 +44,10 @@ while gameOn:
         scores.gameOver()
     
     # when snake collides to any other part of the body
-    for i in snake.snakeSegments:
-        if i == snake.head: # off course, distance between head and head is 0, that is less than 10
-            pass
-        elif snake.head.distance(i) < 10: # if the distance between head and any other segment is less than 10
+    for i in snake.snakeSegments[1:]: # slicing the list
+        # skipped first item, because the distance between head and head is 0, that is less than 10
+        if snake.head.distance(i) < 10: # if the distance between head and any other segment is less than 10
             gameOn = False
             scores.gameOver()
 
 screen.exitonclick()
-
-
-
-
-#TODO: create a snake body
-#TODO: move the snake
-#TODO: create snake food
-#TODO: detect collision with food
-#TODO: create a scoreboard
-#TODO: detect collision with wall
-#TODO: detect collision with tail
